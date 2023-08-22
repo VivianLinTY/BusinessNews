00961067acb605da1743afd053153b839ce74507
Zoe <Zoe_Lin@compal.com>
2023-08-23 07:47:33 +0800

    Add error tests.

--- app/src/androidTest/java/com/example/news/view_model/ArticleUsViewModelTest.kt
+++ app/src/androidTest/java/com/example/news/view_model/ArticleUsViewModelTest.kt
@@ -2,17 +2,20 @@ package com.example.news.view_model
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.internal.runner.junit4.statement.UiThreadStatement
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.news.repository.ArticleRepository
 import com.example.news.response.ArticleResponse
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.verify
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class ArticleUsViewModelTest {
@@ -40,6 +43,24 @@ class ArticleUsViewModelTest {
         mockLiveData.postValue(mockArticleResponse)
 
         verify { mockRepository.getDashBoardNews("us") }
+    }
+
+    @Test
+    fun testGetDashboardNews_Error() {
+        coEvery { mockRepository.getDashBoardNews("us") } throws Exception("Network error")
+        val latch = CountDownLatch(1)
+        UiThreadStatement.runOnUiThread {
+            val observer = Observer<ArticleResponse> { response ->
+                if (response == null) {
+                    latch.countDown()
+                }
+            }
+            viewModel.dashboardNewsResponseLiveData.observeForever(observer)
+            viewModel.onCleared()
+            coVerify { mockRepository.getDashBoardNews("us") }
+            viewModel.dashboardNewsResponseLiveData.removeObserver(observer)
+        }
+        latch.await(2, TimeUnit.SECONDS)
     }
 
     @Test
